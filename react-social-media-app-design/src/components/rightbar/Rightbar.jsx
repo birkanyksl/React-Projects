948bd548b832ff8114bb1ecdfa9328108d679d10
Birkan <birkanyksl613@gmail.com>
2024-04-25 06:17:03 +0300

    Profile Page oluşturuldu

--- react-social-media-app-design/src/components/rightbar/Rightbar.jsx
+++ react-social-media-app-design/src/components/rightbar/Rightbar.jsx
@@ -2,10 +2,10 @@ import "./rightbar.css";
 import { Users } from "../../dummyData";
 import Online from "../online/Online";
 
-const Rightbar = () => {
-  return (
-    <div className="rightbar">
-      <div className="rightbarWrapper">
+const Rightbar = ({ profile }) => {
+  const HomeRightBar = () => {
+    return (
+      <>
         <div className="birthdayContainer">
           <img className="birthdayImg" src="assets/gift.png" alt="" />
           <span className="birthdayText">
@@ -20,6 +20,87 @@ const Rightbar = () => {
             <Online key={u.id} user={u} />
           ))}
         </ul>
+      </>
+    );
+  };
+
+  const ProfileRightBar = () => {
+    return (
+      <>
+        <h4 className="rightbarTitle">User Information</h4>
+        <div className="rightbarInfo">
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">City:</span>
+            <span className="rightbarInfoValue">New York</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">From:</span>
+            <span className="rightbarInfoValue">Madrid</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">Relationship:</span>
+            <span className="rightbarInfoValue">Single</span>
+          </div>
+        </div>
+        <h4>User Friends</h4>
+        <div className="rightbarFollowings">
+          <div className="rightbarFollowing">
+            <img
+              className="rightbarFollowingImg"
+              src="assets/person/2.jpeg"
+              alt=""
+            />
+            <span className="rightbarFollowingName">Brad Pit</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              className="rightbarFollowingImg"
+              src="assets/person/3.jpeg"
+              alt=""
+            />
+            <span className="rightbarFollowingName">Brad Pit</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              className="rightbarFollowingImg"
+              src="assets/person/4.jpeg"
+              alt=""
+            />
+            <span className="rightbarFollowingName">Brad Pit</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              className="rightbarFollowingImg"
+              src="assets/person/5.jpeg"
+              alt=""
+            />
+            <span className="rightbarFollowingName">Brad Pit</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              className="rightbarFollowingImg"
+              src="assets/person/6.jpeg"
+              alt=""
+            />
+            <span className="rightbarFollowingName">Brad Pit</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              className="rightbarFollowingImg"
+              src="assets/person/7.jpeg"
+              alt=""
+            />
+            <span className="rightbarFollowingName">Brad Pit</span>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <ProfileRightBar />
       </div>
     </div>
   );
---
+++ react-social-media-app-design/src/pages/profile/Profile.jsx
@@ -0,0 +1,42 @@
+import Rightbar from "../../components/rightbar/Rightbar";
+import Siderbar from "../../components/sidebar/Siderbar";
+import Topbar from "../../components/topbar/Topbar";
+import Feed from "../../components/feed/Feed";
+import "./profile.css";
+
+const Profile = () => {
+  return (
+    <>
+      <Topbar />
+      <div className="profile">
+        <Siderbar />
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img
+                className="profileCoverImg"
+                src="assets/post/3.jpeg"
+                alt=""
+              />
+              <img
+                className="profileUserImg"
+                src="assets/person/1.jpeg"
+                alt=""
+              />
+            </div>
+            <div className="profileInfo">
+              <h4 className="profileInfoName">Birkan Yüksel</h4>
+              <span className="profileInfoDesc">Hello, My Friends</span>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Feed />
+            <Rightbar profile />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Profile;
