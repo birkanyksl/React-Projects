fc90af4602ee2ac8872b64f76f22aea547b7e776
Birkan <birkanyksl613@gmail.com>
2024-05-02 07:18:54 +0300

    multer file upload

--- social-media-app/api/index.js
+++ social-media-app/api/index.js
@@ -12,6 +12,8 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const multer = require("multer");
+const path = require("path");
 
 dotenv.config();
 
@@ -19,11 +21,30 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
   console.log("Connected to MongoDB");
 });
 
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
 // middleware'leri ekle
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer(storage);
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploaded successfully");
+  } catch (error) {
+    console.log(error);
+  }
+});
 // rotaları tanımla
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
