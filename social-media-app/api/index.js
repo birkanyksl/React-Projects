16caddc7f286604e62e9930910695d5b1ded39ad
Birkan <birkanyksl613@gmail.com>
2024-05-09 17:41:44 +0300

    chat app rest api

--- social-media-app/api/index.js
+++ social-media-app/api/index.js
@@ -11,6 +11,8 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const conversationRoute = require("./routes/conversations");
+const messageRoute = require("./routes/messages");
 const multer = require("multer");
 const path = require("path");
 
@@ -49,6 +51,8 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
 
 // server'ı dinle
 app.listen(8800, () => {
