2e4c8bf756659ed224462c5e518dc9ba4f6c1368
Birkan <birkanyksl613@gmail.com>
2024-05-09 14:42:00 +0300

    messenger component

--- social-media-app/client/src/pages/messenger/Messenger.jsx
+++ social-media-app/client/src/pages/messenger/Messenger.jsx
@@ -1,11 +1,40 @@
 import "./messenger.css";
 import Topbar from "../../components/topbar/Topbar";
+import Conversation from "../../components/consersations/Conversation";
+import Message from "../../components/message/Message";
 
 const Messenger = () => {
   return (
     <>
       <Topbar />
-      <div className="messenger">Messenger</div>
+      <div className="messenger">
+        <div className="chatMenu">
+          <div className="chatMenuWrapper">
+            <input
+              type="text"
+              placeholder="Search for friends"
+              className="chatMenuInput"
+            />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+          </div>
+        </div>
+        <div className="chatBox">
+          <div className="chatBoxWrapper">
+            <div className="chatBoxTop">
+              <Message />
+              <Message />
+              <Message />
+            </div>
+            <div className="chatBoxBottom"></div>
+          </div>
+        </div>
+        <div className="chatOnline">
+          <div className="chatOnlineWrapper">online</div>
+        </div>
+      </div>
     </>
   );
 };
