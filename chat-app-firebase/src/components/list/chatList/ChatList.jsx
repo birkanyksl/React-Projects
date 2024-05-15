b73e7a1a0cff55720517c91c53b7baaddbd30b5b
Birkan <birkanyksl613@gmail.com>
2024-05-15 19:17:09 +0300

    app container

---
+++ chat-app-firebase/src/components/list/chatList/ChatList.jsx
@@ -0,0 +1,17 @@
+import "./chatList.css";
+
+const Chatlist = () => {
+  return (
+    <div className="chatList">
+      <div className="search">
+        <div className="searchBar">
+          <img src="./search.png" alt="" />
+          <input type="text" placeholder="Search" />
+        </div>
+        <img className="add" src="./plus.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Chatlist;
