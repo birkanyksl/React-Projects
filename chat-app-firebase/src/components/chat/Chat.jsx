4454d37a4c003c6b46bab0cae5a664184c3ac03f
Birkan <birkanyksl613@gmail.com>
2024-05-17 01:37:13 +0300

    chat component
    profile
    chat area
    text area
    emoji picker

--- chat-app-firebase/src/components/chat/Chat.jsx
+++ chat-app-firebase/src/components/chat/Chat.jsx
@@ -1,7 +1,122 @@
+import { useState } from "react";
 import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
 
 const Chat = () => {
-  return <div className="chat">Chat</div>;
+  const [open, setOpen] = useState(false);
+  const [text, setText] = useState("");
+
+  const handleEmoji = (e) => {
+    setText((prevstate) => prevstate + e.emoji);
+    setOpen(false);
+  };
+
+  return (
+    <div className="chat">
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Birkan Yksl</span>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+
+      <div className="center">
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia
+              dolores similique perferendis sit eaque. Amet excepturi optio
+              necessitatibus earum cupiditate!
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia
+              dolores similique perferendis sit eaque. Amet excepturi optio
+              necessitatibus earum cupiditate!
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia
+              dolores similique perferendis sit eaque. Amet excepturi optio
+              necessitatibus earum cupiditate!
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia
+              dolores similique perferendis sit eaque. Amet excepturi optio
+              necessitatibus earum cupiditate!
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+        <div className="message own">
+          <div className="texts">
+            <img
+              src="https://images.pexels.com/photos/3101084/pexels-photo-3101084.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia
+              dolores similique perferendis sit eaque. Amet excepturi optio
+              necessitatibus earum cupiditate!
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input
+          type="text"
+          placeholder="Type a message..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <div className="emoji">
+          <img
+            src="./emoji.png"
+            alt=""
+            onClick={() => setOpen((prevstate) => !prevstate)}
+          />
+          <div className="picker">
+            <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+          </div>
+        </div>
+        <button className="sendButton">Send </button>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
