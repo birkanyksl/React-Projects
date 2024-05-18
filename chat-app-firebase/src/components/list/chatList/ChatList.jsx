5dc2199a6bc7a58e916f8c359bfc22695b0777bc
Birkan <birkanyksl613@gmail.com>
2024-05-18 15:34:59 +0300

    fetch userchat

--- chat-app-firebase/src/components/list/chatList/ChatList.jsx
+++ chat-app-firebase/src/components/list/chatList/ChatList.jsx
@@ -1,9 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./chatList.css";
 import AddUser from "./addUser/AddUser";
+import useUserStore from "../../../lib/userStore";
+import { doc, getDoc, onSnapshot } from "firebase/firestore";
+import { db } from "../../../lib/firebase";
 
 const Chatlist = () => {
   const [addMode, setAddMode] = useState(false);
+  const [chats, setChats] = useState([]);
+
+  const { currentUser } = useUserStore();
+
+  useEffect(() => {
+    const unSub = onSnapshot(
+      doc(db, "userchats", "currentUser.id"),
+      async (res) => {
+        const items = res.data().chats;
+
+        const promises = items.map(async (item) => {
+          const userDocRef = doc(db, "users", item.receiverId);
+          const userDocSnap = await getDoc(userDocRef);
+
+          const user = userDocSnap.data();
+
+          return { ...item, user };
+        });
+        const chatData = await Promise.all(promises);
+        setChats(chatData.sort((a, b) => b.updatedAt - a.updatedAt));
+      }
+    );
+
+    return () => {
+      unSub();
+    };
+  }, [currentUser.id]);
+
   return (
     <div className="chatList">
       <div className="search">
@@ -18,85 +49,16 @@ const Chatlist = () => {
           onClick={() => setAddMode((prevState) => !prevState)}
         />
       </div>
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Birkan Yksl</span>
-          <p>hello</p>
+      {chats.map((chat) => (
+        <div className="item" key={chat.chatId}>
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Birkan Yksl</span>
+            <p>chat.lastMessage</p>
+          </div>
         </div>
-      </div>
+      ))}
 
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Birkan Yksl</span>
-          <p>hello</p>
-        </div>
-      </div>
-
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Birkan Yksl</span>
-          <p>hello</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Birkan Yksl</span>
-          <p>hello</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Birkan Yksl</span>
-          <p>hello</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Birkan Yksl</span>
-          <p>hello</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Birkan Yksl</span>
-          <p>hello</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Birkan Yksl</span>
-          <p>hello</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Birkan Yksl</span>
-          <p>hello</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Birkan Yksl</span>
-          <p>hello</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Birkan Yksl</span>
-          <p>hello</p>
-        </div>
-      </div>
       {addMode && <AddUser />}
     </div>
   );
