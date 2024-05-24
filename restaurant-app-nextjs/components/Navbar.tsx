79928ac1c046bde8a2479ade31c096909ea4d6e7
Birkan <birkanyksl613@gmail.com>
2024-05-24 05:55:55 +0300

    navbar menu and footer design

--- restaurant-app-nextjs/components/Navbar.tsx
+++ restaurant-app-nextjs/components/Navbar.tsx
@@ -1,7 +1,44 @@
 import React from "react";
+import Menu from "./Menu";
+import Link from "next/link";
+import CartIcon from "./CartIcon";
+import Image from "next/image";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const user = false;
+
+  return (
+    <div className="h-12 text-bg-gold p-4 flex items-center justify-between border-b-2 border-b-bg-gold uppercase md:h-24 lg:px-8 xl:px-36">
+      {/* LEFT LINKS */}
+      <div className="hidden md:flex gap-4 flex-1">
+        <Link href="/">Homepage</Link>
+        <Link href="/menu">Menu</Link>
+        <Link href="/">Contact</Link>
+      </div>
+      {/* LOGO */}
+      <div className="text-xl md:font-bold flex-1 md:text-center">
+        <Link href="/">Better Days </Link>
+      </div>
+      {/* MOBILE MENU */}
+      <div className="md:hidden">
+        <Menu />
+      </div>
+
+      {/* RIGHT LINKS */}
+      <div className="hidden md:flex gap-4 items-center justify-end flex-1">
+        <div className="md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer px-1 bg-red-600 rounded-md">
+          <Image src="/phone.png" alt="" width={20} height={20} />
+          <span className="text-white">232 758 1721</span>
+        </div>
+        {!user ? (
+          <Link href="/login">Login</Link>
+        ) : (
+          <Link href="/orders">Orders</Link>
+        )}
+        <CartIcon />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- restaurant-app-nextjs/components/Notification.tsx
+++ restaurant-app-nextjs/components/Notification.tsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
 import React from "react";
 
 const Notification = () => {
-  return <div className="bg-red-600 h-10 w-10">Notification</div>;
+  return (
+    <div className="h-12 bg-gold text-white px-4 py-4 flex gap-2 items-center justify-center text-center text-sm md:text-base cursor-pointer">
+      <Image src="/notification.png" alt="" width={20} height={20} />
+      <span>Enjoy exclusive discounts on your favorite items today</span>
+    </div>
+  );
 };
 
 export default Notification;
