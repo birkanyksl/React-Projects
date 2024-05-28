d22473b107de9cb9ecabdc39411cb9da41548f79
Birkan <birkanyksl613@gmail.com>
2024-05-28 06:54:32 +0300

    Update page.tsx

--- restaurant-app-nextjs/app/menu/[category]/page.tsx
+++ restaurant-app-nextjs/app/menu/[category]/page.tsx
@@ -5,22 +5,22 @@ import React from "react";
 
 const CategoryPage = () => {
   return (
-    <div className="bg-gray-100 flex flex-wrap shadow-lg">
+    <div className="bg-gray-100 flex flex-wrap shadow-lg w-auto p-4">
       {pizzas.map((item) => (
         <Link
-          className="p-4 rounded-lg shadow-lg flex flex-col items-center justify-between gap-2 w-full h-[65vh] md:w-1/2 lg:w-1/3 hover:scale-102 transition-transform duration-300"
+          className="p-4 rounded-lg shadow-lg flex flex-col items-center justify-between h-[65vh] md:w-1/2 lg:w-1/3 hover:scale-102 transition-transform duration-300  "
           href={`/product/${item.id}`}
           key={item.id}
         >
           {/* IMAGE */}
-          <div className="w-full h-[400px] relative">
+          <div className="w-full h-[400px] relative ">
             {item.img && (
               <Image
                 src={item.img}
                 alt=""
                 layout="fill"
                 objectFit="contain"
-                className="rounded-lg aspect-auto"
+                className=" aspect-auto rounded-sm"
               />
             )}
           </div>
@@ -37,7 +37,7 @@ const CategoryPage = () => {
           {/* PRICE AND BUTTON */}
           <div className="flex justify-center gap-16 mt-4 w-full items-center">
             <p className="text-xl text-gray-700 font-bold">${item.price}</p>
-            <button className="px-3 py-1 bg-gray-600 text-white rounded-lg font-normal  hover:scale-105 hover:bg-gray-700 transition-transform duration-300 shadow-myshadow">
+            <button className="px-3 py-1 bg-neutral-700 text-white rounded-lg font-normal  hover:scale-105 hover:bg-neutral-800 transition-transform duration-300 shadow-myshadow">
               Buy now
             </button>
           </div>
@@ -48,32 +48,3 @@ const CategoryPage = () => {
 };
 
 export default CategoryPage;
-
-// <div className="flex flex-wrap  ">
-//   {pizzas.map((item) => (
-//     <Link
-//       className="w-full h-[60vh] shadow-lg sm:w-1/2 lg:w-1/3 p-4 flex items-center "
-//       href={`/product/${item.id}`}
-//       key={item.id}
-//     >
-//       <div className="flex flex-row w-screen h-full gap-1 bg-slate-600">
-//         {/* TEXT CONTAINER */}
-//         <div className="flex flex-col items-start justify-center gap-5 w-1/3 ">
-//           <h1 className="text-base uppercase p-2 font-pacifico font-medium text-gray-800">
-//             {item.title}
-//           </h1>
-//           <h2 className="group-hover:hidden text-lg text-gray-800 font-pacifico">
-//             ${item.price}
-//           </h2>
-//           <button>Click</button>
-//         </div>
-//         {/* IMAGE */}
-//         {item.img && (
-//           <div className="relative w-full h-[80%]">
-//             <Image src={item.img} alt="" fill className="object-contain" />
-//           </div>
-//         )}
-//       </div>
-//     </Link>
-//   ))}
-// </div>
